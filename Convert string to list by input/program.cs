d752622e16b2d88f4b29e70cb9d038b99aa3d22f
Saikat Das Tushar <78821794+Saikat30101997@users.noreply.github.com>
2021-03-09 16:03:23 +0600

    Add files via upload

---
+++ Convert string to list by input/program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace lambda
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string h = Console.ReadLine();
+            var intList = new List<long>(Array.ConvertAll(h.Split(' '), Convert.ToInt64));
+
+
+
+
+
+        }
+    }
+
+}

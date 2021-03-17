83b0c6009cc6634b4e2ee36db0d24920863182ae
Saikat Das Tushar <78821794+Saikat30101997@users.noreply.github.com>
2021-03-17 13:06:42 -0700

    Add files via upload

---
+++ Collection Initializer and how can get user input for student name,id and dept and add in all things in List/program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+namespace Delegates
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Dept { get; set; }
+    }
+   class Program {
+        static void Main(string[] args)
+        {
+            List<Student> std = new List<Student>()
+            {
+                new Student(){Id=1,Name="Saikat",Dept="CSE" },
+                new Student(){Id=2,Name="Shweta",Dept="Anthropology"}
+            };
+
+            foreach (var item in std)
+            {
+                Console.WriteLine(item.Name+" "+item.Dept);
+            }
+            Console.WriteLine("\n");
+            std.Add(new Student() { Id = 3, Name = "Tushar", Dept = "CSE" });
+
+            foreach (var item in std)
+            {
+                Console.WriteLine(item.Name + " " + item.Dept);
+            }
+            Console.WriteLine("\n");
+            for (int i = 1; i <= 2; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                string y = Console.ReadLine();
+                string z = Console.ReadLine();
+
+                std.Add(new Student() { Id = x, Name = y, Dept = z });
+
+            
+            }
+
+            foreach (var item in std)
+            {
+                Console.WriteLine(item.Name + " " + item.Dept);
+            }
+
+        }
+
+
+
+    }
+}
+
